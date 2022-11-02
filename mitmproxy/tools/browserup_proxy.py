b33184c333eb42d5ef17a94b4dc470d5f03df14d
Iries Hu <irieshu@Iriess-MacBook-Pro.local>
2022-11-02 15:12:40 +0800

    Fix: browserup works with mitm v9

--- mitmproxy/tools/browserup_proxy.py
+++ mitmproxy/tools/browserup_proxy.py
@@ -2,6 +2,7 @@
 # Keep an eye on dump.py for updates to incorporate
 
 from mitmproxy import addons
+from mitmproxy.addons import errorcheck
 from mitmproxy import options
 from mitmproxy import master
 from mitmproxy.addons import dumper, termlog, keepserving, readfile
@@ -9,13 +10,13 @@ from mitmproxy.addons.browserup import har_capture_addon, \
     browserup_addons_manager, latency_addon
 
 
-class ErrorCheck:
-    def __init__(self):
-        self.has_errored = False
+# class ErrorCheck:
+#     def __init__(self):
+#         self.has_errored = False
 
-    def add_log(self, e):
-        if e.level == "error":
-            self.has_errored = True
+#     def add_log(self, e):
+#         if e.level == "error":
+#             self.has_errored = True
 
 
 class BrowserupProxyMaster(master.Master):
@@ -27,7 +28,7 @@ class BrowserupProxyMaster(master.Master):
             with_dumper=True,
     ) -> None:
         super().__init__(options)
-        self.errorcheck = ErrorCheck()
+        # self.errorcheck = ErrorCheck()
         if with_termlog:
             self.addons.add(termlog.TermLog())
         self.addons.add(*addons.default_addons())
@@ -40,5 +41,6 @@ class BrowserupProxyMaster(master.Master):
         self.addons.add(
             keepserving.KeepServing(),
             readfile.ReadFileStdin(),
-            self.errorcheck
+            errorcheck.ErrorCheck()
+            # self.errorcheck
         )
